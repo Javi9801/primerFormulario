05632876bea3b598fd58ecc1963564c77a63227b
Javier Jimenez Diaz <jjimdia0908@g.educaand.es>
2021-10-06 09:00:52 +0200

    Validar con Array

--- procesa.php
+++ procesa.php
@@ -1,17 +1,14 @@
 <?php
-if(validaFormulario($_POST["numero1"],$_POST["numero2"])){
-
-
+$errores = validar($_POST["numero1"], $_POST["numero2"]);
+if(count($errores=0)){
     if(isset($_POST["sumar"])){
         $operacion = $_POST["numero1"] + ($_POST["numero2"]);
         header('Location: formulario.php?suma='.$operacion);
     } else {
-
         if(isset($_POST["restar"])){
             $operacion = $_POST["numero1"] - ($_POST["numero2"]);
             header('Location: formulario.php?resta='.$operacion);
         } else {
-
             if(isset($_POST["multiplicar"])){
                 $operacion = $_POST["numero1"] * ($_POST["numero2"]);
                 header('Location: formulario.php?mul='.$operacion);  
@@ -22,7 +19,11 @@ if(validaFormulario($_POST["numero1"],$_POST["numero2"])){
             }
         }
     }
-        
+} else{
+    foreach($errores as $v){
+        echo $v;
+    }
+} 
 
 
 function validaFormulario($num1, $num2){
@@ -31,62 +32,27 @@ function validaFormulario($num1, $num2){
     }
 }
 
+function validar($num1, $num2){
+    $errores = [];
 
-// <?php
-// $errores = validar($_POST["numero1"], $_POST["numero2"]);
-// if(count($errores=0)){
-//     if(isset($_POST["sumar"])){
-//         $operacion = $_POST["numero1"] + ($_POST["numero2"]);
-//         header('Location: formulario.php?suma='.$operacion);
-//     } else {
-//         if(isset($_POST["restar"])){
-//             $operacion = $_POST["numero1"] - ($_POST["numero2"]);
-//             header('Location: formulario.php?resta='.$operacion);
-//         } else {
-//             if(isset($_POST["multiplicar"])){
-//                 $operacion = $_POST["numero1"] * ($_POST["numero2"]);
-//                 header('Location: formulario.php?mul='.$operacion);  
-//             } else {
-//                 // Redirección
-//                 $error = "Error no se puede hacer la operacion";
-//                 header('Location: formulario.php?error='.$error);
-//             }
-//         }
-//     }
-// } else{
-//     foreach($errores as $v){
-//         echo $v;
-//     }
-// } 
+    if(is_numeric($num1)){
+        $errores[$num1] = "Error, primer parametro no numerico";
+    } else {
+        if(is_numeric($num2)){
+            $errores[$num2] = "Error, segundo parametro no numerico";
+        } else{
+            if(isset($num1)){
+                $errores[$num2] = "Error, primer parametro nulo";
+            } else {
+                if(isset($num2)){
+                    $errores[$num2] = "Error, segundo parametro nulo";
+                }
+            }
+        }
+    }
 
-
-// function validaFormulario($num1, $num2){
-//     if(is_numeric($num1) && is_numeric($num2) && isset($num1) && isset($num2)){
-//         return true;
-//     }
-// }
-
-// function validar($num1, $num2){
-//     $errores = [];
-
-//     if(is_numeric($num1)){
-//         $errores[$num1] = "Error, primer parametro no numerico";
-//     } else {
-//         if(is_numeric($num2)){
-//             $errores[$num2] = "Error, segundo parametro no numerico";
-//         } else{
-//             if(isset($num1)){
-//                 $errores[$num2] = "Error, primer parametro nulo";
-//             } else {
-//                 if(isset($num2)){
-//                     $errores[$num2] = "Error, segundo parametro nulo";
-//                 }
-//             }
-//         }
-//     }
-
-//     return $errores;
+    return $errores;
     
 
-// }
+}
 ?>
